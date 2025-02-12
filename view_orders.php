fce1f5157a412695665618cd666639372cdc6dd7
tsewang-ghale <Tsewang.g@ou.edu>
2025-02-12 01:44:00 -0600

    Update view_orders.php

--- view_orders.php
+++ view_orders.php
@@ -1,15 +1,8 @@
-<!-- view_orders.php -->
 <?php
-// Display stored orders
-$file = fopen("orders.csv", "r");
-if ($file !== FALSE) {
-    echo "<table class='table table-bordered mt-4'><tr><th>Name</th><th>Household Size</th><th>Food Selection</th><th>Suggestions</th></tr>";
-    while (($data = fgetcsv($file)) !== FALSE) {
-        echo "<tr><td>{$data[0]}</td><td>{$data[1]}</td><td>{$data[2]}</td><td>{$data[3]}</td></tr>";
-    }
-    echo "</table>";
-    fclose($file);
+echo "<h2>All Orders</h2>";
+if (file_exists("orders.txt")) {
+    echo nl2br(file_get_contents("orders.txt"));
 } else {
-    echo "<p>No orders found.</p>";
+    echo "No orders yet.";
 }
 ?>
